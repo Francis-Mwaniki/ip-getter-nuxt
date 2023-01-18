c17d028be5584e3ec608b91a4f378bf1d43796be
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-18 13:15:01 +0300

    update ip page

--- pages/ip.vue
+++ pages/ip.vue
@@ -4,7 +4,7 @@
         <span class="absolute top-0 right-0 block w-10 h-5 -mt-2 -mr-4 text-xs text-center text-gray-600 transform rotate-45 bg-white rounded shadow-md cursor-pointer hover:bg-gray-100">ip</span>
         <div class="w-1/2 ml-4 bg-cover m-1" style="background-image:url('https://media.istockphoto.com/id/1440433765/photo/users-accessing-website-through-virtual-private-network-to-hide-the-internet-traffic-online.jpg?b=1&s=170667a&w=0&k=20&c=kQ19_h9cR9hz75p6-RGIoxpIEWHL9KjYB-mOBdA8Pks=')">
         </div>
-        <div class="flex flex-col justify-between w-1/2 px-4 space-y-2">
+        <div class="flex flex-col justify-between w-1/2 px-4 space-y-10">
             <div>
                 <h1 class=" text-sm font-bold leading-tight text-red-800">
                   Your ip address is currently  
